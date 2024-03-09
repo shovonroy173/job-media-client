19cfb4387ff2535d323b5059c8590d5b2af36ff3
shovonroy173 <shovonroy2003@gmail.com>
2024-03-09 20:54:52 +0530

    6th commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASn_CvBIR2AlodX4guSkAtQO9YIMWoWB4",
+  authDomain: "jobs-media-82202.firebaseapp.com",
+  projectId: "jobs-media-82202",
+  storageBucket: "jobs-media-82202.appspot.com",
+  messagingSenderId: "134631112457",
+  appId: "1:134631112457:web:1787e8b255da2c8f435dd8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/redux/apiCalls.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import {
+  loginFailure,
+  loginStart,
+  loginSuccess,
+  registerFailure,
+  registerStart,
+  registerSuccess,
+} from "./authRedux";
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart());
+
+  try {
+    const res = await axios.post("http://localhost:5000/api/auth/login", user);
+    console.log("LINE AT 10", res.data);
+    dispatch(loginSuccess(res.data));
+    return res.data;
+  } catch (error) {
+    dispatch(loginFailure());
+    return error;
+  }
+};
+
+export const register = async (dispatch, user) => {
+  dispatch(registerStart());
+  console.log(user);
+  try {
+    const res = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      user
+    );
+    console.log(res.data);
+    dispatch(registerSuccess(res.data));
+  } catch (error) {
+    dispatch(registerFailure());
+  }
+};
